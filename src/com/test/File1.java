bb6d9de6122c650b8af99bd209a1abd139e8db4e
Abdul Azeem <abdulazeem.au@gmail.com>
2019-02-10 14:09:34 +0530

    commit the local branches

--- src/com/test/File1.java
+++ src/com/test/File1.java
@@ -1,12 +1,7 @@
 package com.test;
 
 public class File1 {
-
-	
 	 public static void main(String[] args) {
-		  
-		 System.out.println(" I am from File1 system");
+ 		 System.out.println(" I am from File1 system");
 	}
-	
-	
 }
---
+++ src/com/test/MasterBranchClass.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class MasterBranchClass {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Please set the local address");
+	}
+}
